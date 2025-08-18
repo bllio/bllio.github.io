8bff547c9a13041571ebf5a31b176ae95e36801c
bllio <89642563+bllio@users.noreply.github.com>
2025-08-18 19:28:20 +0800

    Remove back-to-top button
    I found that it is not really necessary since the page is not long even
    enough to warrant it.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import { setupPage } from './modules/page';
 import { setupHeading } from './modules/heading';
 import { setupNavbar } from './modules/navbar';
 import { setupEmail } from './modules/email';
-import { setupScroll } from './modules/scroll';
 
 // Initialize modules and event handlers.
 setupPage();
 setupHeading();
 setupNavbar();
 setupEmail();
-setupScroll();
